d3abb5ff3b30981292e12185fdb7d1fb88715354
tiburon-blanco <antolini.martin@gmail.com>
2022-08-20 14:24:02 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio2.py
+++ UNIDAD2-OPERADORES/ejercicio2.py
@@ -1,2 +1,15 @@
 # Leer 2 valores por TECLADO e imprimir la suma de ambos
 # Usar la funcion input() cuando se pide leer por TECLADO
+from re import A
+
+num1=0
+num2=0
+suma=0
+
+print("bienvenido chinguenguencha")
+print("ingresa un numero: ")
+num1=int(input())
+print("ingresa otro numero: ")
+num2=int(input())
+suma=num1+num2
+print("El resultado de la suma es" ,suma)
