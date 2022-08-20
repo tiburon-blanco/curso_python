4a3460a3a3f52e5053ea10194f3c168639e32179
tiburon-blanco <antolini.martin@gmail.com>
2022-08-19 21:41:14 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio1.py
+++ UNIDAD2-OPERADORES/ejercicio1.py
@@ -1,2 +1,12 @@
 # Dado los valores A  = 10 y B = 2
 # Crear las 4 operaciones aritméticas (suma, resta, división y multiplicación) e imprimir el resultado de cada operacion segun A y B
+A=10
+B=2
+suma=A+B
+print(suma)
+resta=A-B
+print(resta)
+division=A/B
+print(division)
+multiplicacion=A*B
+print(multiplicacion)
