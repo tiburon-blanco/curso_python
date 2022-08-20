16559bd1fccafd640ade746f4887871914e65fea
tiburon-blanco <antolini.martin@gmail.com>
2022-08-20 11:43:20 -0300

    ..

--- UNIDAD1-VARIABLE/ejercicio8.py
+++ UNIDAD1-VARIABLE/ejercicio8.py
@@ -1,18 +1,7 @@
 # Leer por TECLADO el nombre e imprimir un saludo.
 # Por ejemplo: Si se lee el valor "Juan", se debe imprimir: Hola Juan, como estas.
 # Para leer un valor por teclado usar la función: input()
-<<<<<<< HEAD
 
-nombre=input("escriba su nombre")
-print("Hola"+ nombre + ",como estas.")
-
-
-
-
-
-
-
-=======
-nombre = input("Ingrese su nombre:")
-print("Hola " + nombre + ", como estas.")
->>>>>>> e62676096e6a96a816db146832e9c196b4ac5c66
+nombre=input("escriba su nombre: ")
+print("Hola "+ nombre + ", como estas.")
+ 
