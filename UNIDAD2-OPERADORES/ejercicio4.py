1390486e1c4340fbb7d79459ffe982841ae2246c
tiburon-blanco <antolini.martin@gmail.com>
2022-08-20 14:43:32 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio4.py
+++ UNIDAD2-OPERADORES/ejercicio4.py
@@ -1,1 +1,9 @@
 # Escribir un programa que pregunte la edad una familia TIPO y calcule su promedio
+P=int(input("Ingrese la edad del Padre: "))
+M=int(input("Ingrese la edad de la Madre: "))
+H1=int(input("Ingrese la edad del Hije mayor: "))
+H2=int(input("Ingrese la edad del Hije menor: "))
+
+promedio=(P+M+H1+H2)/4
+print("La edad promedio es:",promedio)
+
