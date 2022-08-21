ef19e31a81adc6f834dde62885cd8540cd8bb9e3
tiburon-blanco <antolini.martin@gmail.com>
2022-08-21 15:44:05 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio7.py
+++ UNIDAD2-OPERADORES/ejercicio7.py
@@ -1,2 +1,12 @@
 # Escriba un programa que pida una distancia en pies y pulgadas y que escriba esa distancia en centímetros.
 # un pie son doce pulgadas y una pulgada son 2,54 cm.
+pulgada=2.54
+pie=12*pulgada
+
+distancia_1=float(input("ingrese la distancia en pies: "))
+distancia_2=float(input("ingrese la distancia en pulgadas: "))
+
+
+dist_cm=(distancia_1*(pulgada*12))+(distancia_2*pulgada)
+
+print("La conversion de pies y pulgadas es de", dist_cm, "centimetros")
