d72da1602de2020da5c2cd926c01f4bc0efea854
tiburon-blanco <antolini.martin@gmail.com>
2022-08-21 15:22:16 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio6.py
+++ UNIDAD2-OPERADORES/ejercicio6.py
@@ -1,3 +1,9 @@
 # Calcular la media e imprimir de 2 numeros que se ingresan por TECLADO
 # Recordar CASTEAR a entero al leer el valor, CASTEAR es comvertir de un tipo a otro porque por teclado es todo STRING
 # int(input("Ingrese el primer número: "))
+
+n1=int(input("Ingrese el primer número: "))
+n2=int(input("Ingrese el segundo número: "))
+media=(n1+n2)/2
+print("La media es: ", media)
+
