b3a06c6b51b54dba607ed0e98623a46767f627d3
tiburon-blanco <antolini.martin@gmail.com>
2022-08-21 13:22:45 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio5.py
+++ UNIDAD2-OPERADORES/ejercicio5.py
@@ -1,1 +1,5 @@
 # Ingrear por TECLADO 1 palabra, y multiplicarla por 5, cual es su resultado?
+
+nombre=input("coloque su nombre")
+resultado= nombre*5
+print("el producto es 5 veces: :", resultado)
