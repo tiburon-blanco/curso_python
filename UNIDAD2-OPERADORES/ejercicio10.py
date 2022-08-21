c51d227512ba629c72fc09eeb49cbffa6303eff1
tiburon-blanco <antolini.martin@gmail.com>
2022-08-21 18:14:40 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio10.py
+++ UNIDAD2-OPERADORES/ejercicio10.py
@@ -1,2 +1,7 @@
 # Escribir un programa que pida una cantidad de minutos y que escriba cuántos segundos son.
 
+min=float(input("Ingrese la cantidad de minutos: "))
+
+segundos=min*60
+
+print("La cantidad de segundos de los minutos ingresados es de:",segundos)
