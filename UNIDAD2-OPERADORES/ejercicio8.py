7cc8f8271e87d3aae479e8609d9243464674a5be
tiburon-blanco <antolini.martin@gmail.com>
2022-08-21 15:51:24 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio8.py
+++ UNIDAD2-OPERADORES/ejercicio8.py
@@ -1,2 +1,8 @@
 # Escriba un programa que pida una temperatura en grados Fahrenheit y que escriba esa temperatura en grados Celsius.
 # La relación entre grados Celsius (C) y grados Fahrenheit (F) es la siguiente: C = (F - 32) / 1,8
+
+temperatura=float(input("Ingrese la temperatura en grados Fahrenheit: "))
+
+celsius=float((temperatura-32)/1.8)
+
+print("La conversion de grados Fahrenheit a Celsius es la siguiente: ", celsius)
