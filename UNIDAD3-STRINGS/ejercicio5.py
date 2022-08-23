c73dcea8ee51a614af11b9aee5fc96f3d4e9d1bf
tiburon-blanco <antolini.martin@gmail.com>
2022-08-23 15:47:03 -0300

    .

--- UNIDAD3-STRINGS/ejercicio5.py
+++ UNIDAD3-STRINGS/ejercicio5.py
@@ -2,3 +2,6 @@
 # Utilizar la funcion replace() 
 # Crear el string "Hola como te va, el jueves jugamos al futbol y al tenis"
 # Reemplazar las "futbol" por una "basquet" y mostrar en pantalla
+texto=("Hola como te va, el jueves jugamos al futbol y al tenis")
+nuevo_texto=texto.replace("futbol","basquet")
+print(nuevo_texto)
