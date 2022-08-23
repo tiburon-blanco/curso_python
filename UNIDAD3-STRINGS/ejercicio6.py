6ba70bcee7a38ccec2f301927be0b2447250b547
tiburon-blanco <antolini.martin@gmail.com>
2022-08-23 16:02:25 -0300

    .

--- UNIDAD3-STRINGS/ejercicio6.py
+++ UNIDAD3-STRINGS/ejercicio6.py
@@ -3,3 +3,10 @@
 # Se utiliza [x,y] donde "x" es a aprtir de donde, e "y" es hasta donde
 # Crear el string "Hola como te va, el jueves jugamos al futbol y al tenis"
 # Crear un substring con la frase: "como te va" del string anterior
+
+texto="Hola como te va, el jueves jugamos al futbol y al tenis"
+print(texto)
+
+texto_1=texto[5:15]
+
+print(texto_1)
