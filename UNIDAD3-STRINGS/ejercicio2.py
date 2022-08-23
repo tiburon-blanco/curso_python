1466c53597573520b08f85473cdafae089ca1c08
tiburon-blanco <antolini.martin@gmail.com>
2022-08-23 10:04:35 -0300

    .

--- UNIDAD3-STRINGS/ejercicio2.py
+++ UNIDAD3-STRINGS/ejercicio2.py
@@ -1,3 +1,9 @@
 # LONGITUD
 # Para obteter longitud de un string se utiliza la funvion "len()"
 # Ingrear por teclado 1 franse, contar la cantidad de caracteres, mostrar por pantalla la cantidad
+
+frase=input("Ingrese una frase: ")
+
+cantidad=len(frase)
+
+print("La cantidad de caracteres de la frase es de: ",cantidad)
