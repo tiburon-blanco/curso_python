d1e012136ba57026c12a5029cf2a1b68019a80b4
tiburon-blanco <antolini.martin@gmail.com>
2022-08-22 22:24:45 -0300

    .

--- UNIDAD2-OPERADORES/ejercicio12.py
+++ UNIDAD2-OPERADORES/ejercicio12.py
@@ -2,3 +2,18 @@
 # El precio de los clavos es de 0.7 c/u y el de los tornillos de es 1.25 c/u
 # Escribir un programa que simule 2 ventas, preguntado al cliente cuantos clavos y tornillos necesita.
 # Mostrar al final la cantidad de clavos y tornillos vendidos y monto recaudado para cada caso.
+
+precio_C=float(0.7)
+precio_T=float(1.25)
+
+demanda_C=int(input("Ingrese la cantidad de clavos: "))
+demanda_T=int(input("Ingrese la cantidad de tornillos: "))
+
+clavos_total=float(demanda_C*precio_C)
+tornillos_total=float(demanda_T*precio_T)
+
+compra_T=clavos_total+tornillos_total
+
+print("El valor de la compra de clavos es de pesos:",clavos_total)
+print("El valor de la compra de tornillos es de pesos:",tornillos_total)
+print("El valor total de la compra de clavos y tornillos asciende a la suma de pesos: ",compra_T)
