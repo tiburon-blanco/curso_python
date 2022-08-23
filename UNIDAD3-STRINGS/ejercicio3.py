f6b89104807a32bddc60bd5ef2aff18984e9029b
tiburon-blanco <antolini.martin@gmail.com>
2022-08-23 13:15:57 -0300

    .

--- UNIDAD3-STRINGS/ejercicio3.py
+++ UNIDAD3-STRINGS/ejercicio3.py
@@ -1,3 +1,12 @@
 # BUSCAR
 # Ingrear por teclado 2 palabras, se deberá buscar la segunda palabra dentro de la primera e imprimir el resultado de find
 # Que tipo de variable es?
+
+
+frase=input("ingrese una frase: ")
+
+palabra= input("ingrese una palabra: ")
+
+resultado=frase.find(palabra)
+print(type(resultado))
+print(resultado)
