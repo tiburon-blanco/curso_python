610b2501ef387a6608ef92295da28cb7458f24bc
tiburon-blanco <antolini.martin@gmail.com>
2022-08-22 22:58:48 -0300

    .

--- UNIDAD3-STRINGS/ejercicio1.py
+++ UNIDAD3-STRINGS/ejercicio1.py
@@ -1,3 +1,10 @@
 # CONCATENAR
 # Para concatener se usa el +
 # Ingrear por teclado 3 palabras, concatenarlas y mostrar el resultado en pantalla
+
+
+palabra_1=str(input("Ingrese la primera palabra: "))
+palabra_2=str(input("Ingrese la segunda palabra: "))
+palabra_3=str(input("Ingrese la tercera palabra: "))
+
+print("Las palabras ingresadas son: ",palabra_1+" "+palabra_2+" "+palabra_3+".")
