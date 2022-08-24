9b01899496ec754622730b3ecb16080054cfd161
tiburon-blanco <antolini.martin@gmail.com>
2022-08-24 19:39:07 -0300

    .

--- UNIDAD4-LISTAS/ejercicio3.py
+++ UNIDAD4-LISTAS/ejercicio3.py
@@ -1,3 +1,8 @@
 # Contar la cantidad de elementos que tiene una lista
 # Usar count()
 # lista = [3, "HOLA", True, [2, 3, 6]]
+
+lista=[3, "HOLA", True, [2, 3, 6]]
+
+elemento=lista.count([3])
+print(elemento)
