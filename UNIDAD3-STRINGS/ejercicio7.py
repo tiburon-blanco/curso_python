791226574f1efc74ca49ad4a5186668bdae42534
tiburon-blanco <antolini.martin@gmail.com>
2022-08-24 08:58:56 -0300

    .

--- UNIDAD3-STRINGS/ejercicio7.py
+++ UNIDAD3-STRINGS/ejercicio7.py
@@ -1,3 +1,13 @@
 # SUBSTRING o RECORTAR
 # Ingrear por teclado 1 franse y mostrar por pantalla la primera letra
 # Luego mostrar la ultima, para este caso además usa len()
+
+frase=input("(Ingrese una frase: ")
+
+print(frase[0]) #imprimo primer palabra de la frase
+
+
+print(frase[-1])  #imprimo ultima palabra de la frase
+
+#intente con len() me arroja la cantidad de caracteres que tiene y luego queria imprimir el ultimo, pero no lo pude hacer, 
+# me parecia que como la variable se modifica quizas esta era la mejor opcion
