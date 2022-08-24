2ff3f669a64417e823a3dc67a611f2d18faa15a7
tiburon-blanco <antolini.martin@gmail.com>
2022-08-24 18:47:52 -0300

    .

--- UNIDAD4-LISTAS/ejercicio2.py
+++ UNIDAD4-LISTAS/ejercicio2.py
@@ -3,3 +3,11 @@
 # Dada la siguiente lista:
 # lista1 = [3, "HOLA", True, [2, 3, 6]]
 # lista2 = [7, 8, 9, 30]
+
+lista1=[3, "HOLA", True, [2, 3, 6]]
+print("Esta lista: ",lista1, "es heterogena porque contiene distintos tipos de datos")
+
+lista2=[7, 8, 9, 30]
+print("Esta lista: ",lista2, "es homogenea porque contiene distintos tipos de datos")
+
+
