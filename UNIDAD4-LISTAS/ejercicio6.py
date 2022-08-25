7cdb4147f9b3123e04f4bff7faf1af6a3d6e5770
tiburon-blanco <antolini.martin@gmail.com>
2022-08-25 20:39:26 -0300

    .

--- UNIDAD4-LISTAS/ejercicio6.py
+++ UNIDAD4-LISTAS/ejercicio6.py
@@ -2,3 +2,9 @@
 # lista = [["agua", 7, True], [88, 21, 9]]
 # Cuantos elementos tiene la lista?
 # Que tipos de elementos contiene la lista?
+
+lista = [["agua", 7, True], [88, 21, 9]]
+
+print(len(lista))
+print(type(lista))
+
