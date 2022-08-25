13a87852a4d632f3b57ba5757c1a9565432ceb6c
tiburon-blanco <antolini.martin@gmail.com>
2022-08-25 20:48:40 -0300

    .

--- UNIDAD4-LISTAS/ejercicio8.py
+++ UNIDAD4-LISTAS/ejercicio8.py
@@ -3,3 +3,7 @@
 # Dada la siguiente lista:
 # lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
 # En que posicion de la lista se encuentra el elemento "Mundo"
+
+lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
+
+print(lista.index("Mundo"))
