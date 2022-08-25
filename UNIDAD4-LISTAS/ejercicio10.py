f9d61f2ee4d60c7e2dec07425f13da6cd5d2469b
tiburon-blanco <antolini.martin@gmail.com>
2022-08-25 20:59:07 -0300

    .

--- UNIDAD4-LISTAS/ejercicio10.py
+++ UNIDAD4-LISTAS/ejercicio10.py
@@ -2,3 +2,13 @@
 # Dada la siguiente lista:
 # Agregar el elemento 10 a la lista
 # Agregar la lista [7, 2] a la lista
+
+lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
+
+print(lista)
+
+lista.append(10)
+print(lista)
+
+lista.append([7,2])
+print(lista)
