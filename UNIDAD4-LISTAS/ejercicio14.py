11c3901b17fef7f968531d7f0188605bceeccdb8
tiburon-blanco <antolini.martin@gmail.com>
2022-08-26 12:55:05 -0300

    .

--- UNIDAD4-LISTAS/ejercicio14.py
+++ UNIDAD4-LISTAS/ejercicio14.py
@@ -2,3 +2,7 @@
 # Dada la siguiente lista:
 # lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
 # Obtener el último elemento de la lista y mostrar por pantalla
+
+lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
+
+print(lista.pop())
