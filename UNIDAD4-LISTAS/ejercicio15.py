deabdd534e4120e798712eb96c3e5adef8ddd153
tiburon-blanco <antolini.martin@gmail.com>
2022-08-26 12:59:45 -0300

    .

--- UNIDAD4-LISTAS/ejercicio15.py
+++ UNIDAD4-LISTAS/ejercicio15.py
@@ -3,3 +3,14 @@
 # a = "Esto es un string"
 # Convertir a lista con list()
 # Que hace la función list()? 
+
+
+
+a = "Esto es un string"
+
+b=list(a)
+
+print(b)
+
+#La función list() toma una cadena como argumento y la 
+# convierte en una lista. Cada carácter de la cadena se convierte en el elemento individual de la lista.
