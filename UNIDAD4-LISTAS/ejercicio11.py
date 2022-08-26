1050806252c8beb027e45cc084048d153b23c0ee
tiburon-blanco <antolini.martin@gmail.com>
2022-08-26 12:17:53 -0300

    .

--- UNIDAD4-LISTAS/ejercicio11.py
+++ UNIDAD4-LISTAS/ejercicio11.py
@@ -4,3 +4,7 @@
 # Extender la lista con el elemento 10
 # Extender la lista con la lista [7, 2] (Notar la diferencia con el ejercio anterior, osea APPEND)
 # Cual es la diferecnia entre APPEND y EXTEND?
+
+lista=[3, "HOLA", True, [2, 3, 6], "Mundo", 8]
+
+lista.extend([10])
