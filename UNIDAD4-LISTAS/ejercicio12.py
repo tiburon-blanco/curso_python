f76b20d4deee67b5a4c6feaebd3061432db25a53
tiburon-blanco <antolini.martin@gmail.com>
2022-08-26 12:38:50 -0300

    .

--- UNIDAD4-LISTAS/ejercicio12.py
+++ UNIDAD4-LISTAS/ejercicio12.py
@@ -2,3 +2,7 @@
 # Dada la siguiente lista:
 # lista = [14, 22.5, 15, 3.2, 2.1, 6]
 # Ordenar los elementos de una lista e imprimir en pantalla
+
+lista = [14, 22.5, 15, 3.2, 2.1, 6]
+
+print(lista.sort())
