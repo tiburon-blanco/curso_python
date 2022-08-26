ad36f7a4be82f71cf1f7eab627d5528a98d1a0f9
tiburon-blanco <antolini.martin@gmail.com>
2022-08-26 12:48:23 -0300

    .

--- UNIDAD4-LISTAS/ejercicio13.py
+++ UNIDAD4-LISTAS/ejercicio13.py
@@ -2,3 +2,9 @@
 # Dada la siguiente lista:
 # lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
 # Inserta el elemento "5" en la tercera posición de la lista y mostrar la lista por pantalla
+
+lista = [3, "HOLA", True, [2, 3, 6], "Mundo", 8]
+
+lista.insert(3,"E")
+
+print(lista)
