537ffa270667b04a30629aed34d6658f48f32cac
Leo <leocuello@gmail.com>
2022-08-28 11:30:16 -0300

    FIX

--- UNIDAD5-TUPLAS/ejercicio10.py
+++ UNIDAD5-TUPLAS/ejercicio10.py
@@ -1,4 +1,4 @@
 # COUNT (Método)
 # Data la tupla 
 # t = [2, 16, 7, 43, 90, 29, 7, 4, 7, 125, 9]
-# A) Contar la catidad de veces que aparece el número 7 y mostrar por pontalla
+# Contar la catidad de veces que aparece el número 7 y mostrar por pontalla
