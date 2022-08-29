16af2555bb952e02d7f68b79b4e4124bebeee540
Leo <leocuello@gmail.com>
2022-08-28 23:16:14 -0300

    FIX

--- UNIDAD6-SETS/ejercicio7.py
+++ UNIDAD6-SETS/ejercicio7.py
@@ -2,6 +2,6 @@
 # & = Intersessión
 # | = Conjunción
 # Dados los sets: 
-# a = {1, 2, 3, 4,}
-# b = {2, 4, 6, 8,}
+# a = {3, 4, 5, 6}
+# b = {4, 5, 8, 9}
 # Mueste en pantalla Intersessión y la Conjunción
