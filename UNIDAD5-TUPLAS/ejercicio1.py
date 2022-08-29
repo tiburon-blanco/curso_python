8307fdde43ac33c397adf13a90901eb86aac8561
tiburon-blanco <antolini.martin@gmail.com>
2022-08-29 15:32:54 -0300

    .

--- UNIDAD5-TUPLAS/ejercicio1.py
+++ UNIDAD5-TUPLAS/ejercicio1.py
@@ -2,3 +2,11 @@
 # frutas = ("pera", "banana", "manzana")
 # Responder: A nivel sintáctico, que nota con respecto a la lista?
 
+# la diferencia con una lista es que no admite modificaciones, tipo agregar, remover o extender.
+frutas = ("pera", "banana", "manzana")
+
+# la diferencia con respecto a la lista, que estas se escriben entre parentesis, mientras las listas van entre corchetes.
+
+print(frutas)
+
+
