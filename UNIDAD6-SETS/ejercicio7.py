cda6eb1219a0c5550b3b8f9d47f32afb0bf216a0
Leo <leocuello@gmail.com>
2022-08-28 23:20:41 -0300

    FIX

--- UNIDAD6-SETS/ejercicio7.py
+++ UNIDAD6-SETS/ejercicio7.py
@@ -1,7 +1,9 @@
-# OPERADORES de sets o conjuntos & y | 
+# OPERADORES de sets o conjuntos &  |  -
 # & = Intersessión
 # | = Conjunción
+# - = Diferencia
 # Dados los sets: 
 # a = {3, 4, 5, 6}
 # b = {4, 5, 8, 9}
 # Mueste en pantalla Intersessión y la Conjunción
+# Mueste la diferencia entre los sets. Es lo mismo hacer (a-b) que (b-a)
