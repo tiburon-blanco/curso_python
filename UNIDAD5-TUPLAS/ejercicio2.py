90134ee7861e09c44f07d3c56e57ee7fe420f37d
tiburon-blanco <antolini.martin@gmail.com>
2022-08-29 16:50:31 -0300

    .

--- UNIDAD5-TUPLAS/ejercicio2.py
+++ UNIDAD5-TUPLAS/ejercicio2.py
@@ -1,3 +1,8 @@
 # TYPE (Función)
 # Cree un tupla con los nombres de la semana
 # Muestre el tipo en pantalla de los dias de la semana
+
+dias_semana=("Lunes", "Martes", "Miercoles","Jueves","Viernes","SAbado", "Domingo")
+
+print(dias_semana)
+print(type(dias_semana))
