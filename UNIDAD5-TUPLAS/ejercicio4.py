5713f2709e5ff0d5fb3ac510ef1ba291bb9db47c
tiburon-blanco <antolini.martin@gmail.com>
2022-08-30 11:16:32 -0300

    .

--- UNIDAD5-TUPLAS/ejercicio4.py
+++ UNIDAD5-TUPLAS/ejercicio4.py
@@ -1,6 +1,11 @@
 # ANIDAR TUPLAS 
 # Dada la tupla: 
 # tupla = ((1, 2, 3), ("a", "b", "C"))
-# Responder sin programar
-# Cuantos elementos tiene la tupla?
-# De que tipo es cada elemento?
+# Responder sin programar       
+# Cuantos elementos tiene la tupla?   POSEE DOS ELEMENTOS
+# De que tipo es cada elemento?   tuple
+
+tupla = ((1, 2, 3), ("a", "b", "C"))
+print(type(tupla))
+print(len(tupla))
+print(tupla[0])
