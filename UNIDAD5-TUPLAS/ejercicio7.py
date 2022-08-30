18fffc042fa57ff5c6fe826c42a935e4f7c11f25
tiburon-blanco <antolini.martin@gmail.com>
2022-08-30 12:23:20 -0300

    .

--- UNIDAD5-TUPLAS/ejercicio7.py
+++ UNIDAD5-TUPLAS/ejercicio7.py
@@ -1,3 +1,7 @@
 # INDEX (Método)
 # Cree un tupla con los colores del semáforo.
 # Busque el color "rojo" dentro de la tupla e indique su posición
+
+tupla=("Verde","Amarillo","Rojo")
+
+print(tupla.index("Rojo"))
