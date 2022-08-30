b0d2671fdc94fc3f81146b1133b424a0e9756aad
tiburon-blanco <antolini.martin@gmail.com>
2022-08-30 11:01:14 -0300

    ..

--- UNIDAD5-TUPLAS/ejercicio3.py
+++ UNIDAD5-TUPLAS/ejercicio3.py
@@ -1,3 +1,10 @@
 # Cree una tupla con los caractes de la siguiente frese: "Estoy programando". 
 # Cada caracter debe ser un elemento de la tupla
 # Obtenga en segundo elemento de esa tupla y muestre por pantalla
+
+frase=tuple("Estoy programando")
+print(frase)
+print(type(frase))
+
+print(frase[2])
+
