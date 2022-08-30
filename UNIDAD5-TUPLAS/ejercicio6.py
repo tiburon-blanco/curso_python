a8faf6e40dde254cb8604edbbab5774da23383fd
tiburon-blanco <antolini.martin@gmail.com>
2022-08-30 12:20:07 -0300

    .

--- UNIDAD5-TUPLAS/ejercicio6.py
+++ UNIDAD5-TUPLAS/ejercicio6.py
@@ -1,3 +1,9 @@
 # Data la tupla: 
 # tupla = (1, 2, 3)
 # Obtenga el elemento de la posición 5, que sucede, se puede? Responder con un comentario.
+
+tupla = (1, 2, 3)
+
+print(tupla[5])
+
+# no se puede obtener porque no existe la posicion 5, solo hay 3 elementos,
