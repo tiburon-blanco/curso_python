f0262913fe82514d3f701c041b13a8968689666c
tiburon-blanco <antolini.martin@gmail.com>
2022-08-31 10:42:16 -0300

    .

--- UNIDAD5-TUPLAS/ejercicio10.py
+++ UNIDAD5-TUPLAS/ejercicio10.py
@@ -2,3 +2,8 @@
 # Dada la tupla 
 # t = [2, 16, 7, 43, 90, 29, 7, 4, 7, 125, 9]
 # Contar la catidad de veces que aparece el número 7 y mostrar por pontalla
+
+t = (2, 16, 7, 43, 90, 29, 7, 4, 7, 125, 9)
+x=t.count(7)
+
+print(x)
