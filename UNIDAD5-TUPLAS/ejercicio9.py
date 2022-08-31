51078a234c7ea9a843af095e83f37e9ef7ce293c
tiburon-blanco <antolini.martin@gmail.com>
2022-08-31 10:32:59 -0300

    ..

--- UNIDAD5-TUPLAS/ejercicio9.py
+++ UNIDAD5-TUPLAS/ejercicio9.py
@@ -1,2 +1,7 @@
 # Enumere al menos 2 metodos en los cuales la lista tiene y la tupla no
 # No progranar, solo comentario
+
+# la lista tiene el metodo extend y append y la tupla no. La tupla solo admite la busqueda, son inmutables, 
+
+metodos_tupla=input("Ingrese 2 metodos que tiene la lista y la tupla no: ") 
+
