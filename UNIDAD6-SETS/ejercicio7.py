29aa06ec819852f32a0236f933916c4beebb377e
tiburon-blanco <antolini.martin@gmail.com>
2022-09-02 19:01:47 -0300

    .

--- UNIDAD6-SETS/ejercicio7.py
+++ UNIDAD6-SETS/ejercicio7.py
@@ -7,3 +7,19 @@
 # b = {4, 5, 8, 9}
 # Mueste en pantalla Intersessión y la Conjunción
 # Mueste la diferencia entre los sets. Es lo mismo hacer (a-b) que (b-a)
+
+a = {3, 4, 5, 6}
+b = {4, 5, 8, 9}
+
+
+c=a&b
+print("El resultado de la interseccion de los set a y b es: ", c)
+
+c=a|b
+print("El resultado de la conjuncion de los set a y b es: ", c)
+
+c=(a-b)
+print("El resultado de la diferencia de los set a - b es: ", c)
+
+c=(b-a)
+print("El resultado de la interseccion de los set  b-a es: ", c)
