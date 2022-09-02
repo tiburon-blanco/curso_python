d2669e1b4355d119c3c90db7042242ba46df766c
tiburon-blanco <antolini.martin@gmail.com>
2022-09-02 18:51:22 -0300

    .

--- UNIDAD6-SETS/ejercicio6.py
+++ UNIDAD6-SETS/ejercicio6.py
@@ -2,3 +2,12 @@
 # Dado el set: 
 # s = {9, 2, 5, 7, 3, 8}
 # Agregar el número 6 a la lista y eliminar el 7, Luego mostrar en pantalla el set
+
+from audioop import add
+
+
+s = {9, 2, 5, 7, 3, 8}
+
+s.add(6)
+s.remove(7)
+print(s)
