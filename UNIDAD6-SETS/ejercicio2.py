c4fbf4357526d06801d8f646e7571412eaa7cdd6
tiburon-blanco <antolini.martin@gmail.com>
2022-09-02 18:21:01 -0300

    .

--- UNIDAD6-SETS/ejercicio2.py
+++ UNIDAD6-SETS/ejercicio2.py
@@ -3,3 +3,16 @@
 # El segundo que contenga 3 elementos de tipos BOOLEAN
 # El tercero que contenga 4 elementos de tipos INT
 # Mostrar cada set en pantalla
+
+x= ("Martin", "Leo", "Jora")
+x1=set(x)
+print(x1)
+print(type(x1))
+
+
+y={True, False, True }
+print(y)
+print(type(y))
+
+z={12,18,13,17,12,13,18}
+print(z)
