7da883e9f28d48a001a1e6ff696d6a06846f6739
tiburon-blanco <antolini.martin@gmail.com>
2022-09-02 18:25:27 -0300

    .

--- UNIDAD6-SETS/ejercicio3.py
+++ UNIDAD6-SETS/ejercicio3.py
@@ -2,3 +2,10 @@
 # Cree un set heterogeneo (Valores de diferente tipo)
 # Verifique el tipo del set y muestre en pantalla.
 # De que tipo es?
+
+s_et={"Gallardo",12,23,True, False}
+
+print(s_et)
+print(type(s_et))
+
+# Es de tipo Set
