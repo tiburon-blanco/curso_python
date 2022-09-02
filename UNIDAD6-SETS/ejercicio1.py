1ad67f6c86ec40e1feb2acf1b4a2707b67ee9f4e
tiburon-blanco <antolini.martin@gmail.com>
2022-09-02 18:10:54 -0300

    .

--- UNIDAD6-SETS/ejercicio1.py
+++ UNIDAD6-SETS/ejercicio1.py
@@ -4,3 +4,14 @@
 # frutas = {"pera", "banana", "manzana"}
 # Responder: A nivel sintáctico, que nota con respecto a la lista?
 
+
+# Una colección que no contiene elementos duplicados. A difernecia de la lista que si los puede tener.
+
+# A nivel sintacto , la caracteristica son las llaves.
+
+frutas = {"pera", "banana", "manzana"}
+
+print(frutas)
+
+
+
