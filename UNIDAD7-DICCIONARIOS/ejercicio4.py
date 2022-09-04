5552d1171208f285f7bc4f958095ea7b2f455ffb
tiburon-blanco <antolini.martin@gmail.com>
2022-09-03 21:11:04 -0300

    .

--- UNIDAD7-DICCIONARIOS/ejercicio4.py
+++ UNIDAD7-DICCIONARIOS/ejercicio4.py
@@ -4,3 +4,9 @@
 # telefonos = {'Martin': 221738293, 'Leo': 221343423, 'Juan': 2218323478}
 # Agregar el teléfono 221534534 que pertence a Maria
 # Mostra en pantalla todos los teléfonos
+
+telefonos = {'Martin': 221738293, 'Leo': 221343423, 'Juan': 2218323478}
+
+telefonos["Maria"]=221534534
+
+print(telefonos)
