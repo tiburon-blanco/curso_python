39d8f00216609501a344ce0b34d15eb6e1a82a9d
tiburon-blanco <antolini.martin@gmail.com>
2022-09-03 21:19:18 -0300

    .

--- UNIDAD7-DICCIONARIOS/ejercicio6.py
+++ UNIDAD7-DICCIONARIOS/ejercicio6.py
@@ -3,3 +3,7 @@
 # Dado el siguiente diccionario
 # telefonos = {'Martin': 221738293, 'Leo': 221343423, 'Juan': 2218323478}
 # Contar la cantidad de elementos y mostar en pantalla
+
+telefonos = {'Martin': 221738293, 'Leo': 221343423, 'Juan': 2218323478}
+
+print(len(telefonos))
