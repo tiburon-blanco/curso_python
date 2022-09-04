1a23639097b4f95274aca8e08aa8a8bbde735a98
tiburon-blanco <antolini.martin@gmail.com>
2022-09-03 21:30:59 -0300

    .

--- UNIDAD7-DICCIONARIOS/ejercicio8.py
+++ UNIDAD7-DICCIONARIOS/ejercicio8.py
@@ -4,3 +4,16 @@
 # Luego agregar el 31 al diccionario
 # Luego eliminar los 2 primeros
 # Mostrar el diccionario en pantalla
+
+n_primos={"dos": 2,"tres": 3,"Cinco": 5, "Siete": 7,}
+
+print(n_primos)
+
+n_primos["Treinta y uno"]=31
+
+print(n_primos)
+
+n_primos.pop("dos")
+n_primos.pop("tres")
+
+print(n_primos)
