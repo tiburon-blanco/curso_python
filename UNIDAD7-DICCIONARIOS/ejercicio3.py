d81b75cca443058f0bba33d03f44527ce2a177e8
tiburon-blanco <antolini.martin@gmail.com>
2022-09-03 21:06:04 -0300

    .

--- UNIDAD7-DICCIONARIOS/ejercicio3.py
+++ UNIDAD7-DICCIONARIOS/ejercicio3.py
@@ -3,3 +3,7 @@
 # Dado el siguiente diccionario
 # patas = {'humano': 2, 'pulpo': 8, 'perro': 4, 'cienpies': 100, 'gato':4}
 # Cuantas patas tiene el cienpies?
+
+patas = {'humano': 2, 'pulpo': 8, 'perro': 4, 'cienpies': 100, 'gato':4}
+
+print("Un cienpies tiene ", patas["cienpies"], "patas" )
