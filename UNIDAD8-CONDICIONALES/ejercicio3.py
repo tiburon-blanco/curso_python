be32413b67fb6cc4f012d62a5351aa9096a515c0
tiburon-blanco <antolini.martin@gmail.com>
2022-09-06 11:30:51 -0300

    .

--- UNIDAD8-CONDICIONALES/ejercicio3.py
+++ UNIDAD8-CONDICIONALES/ejercicio3.py
@@ -1,2 +1,11 @@
 # Escriba un programa que, dade una nota, indique si el alumno fue aprobado o no
 # Se aprueba con 4
+
+nota=int(input("ingrese la nota del alumno: "))
+
+if nota>4:
+    print("aprobado")
+else:
+    print("desaprobado")  
+
+  
