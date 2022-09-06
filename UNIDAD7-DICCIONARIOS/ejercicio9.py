2bee0bf370d7dc59a744f85b75f54636c6c6e56b
tiburon-blanco <antolini.martin@gmail.com>
2022-09-06 10:57:34 -0300

    .

--- UNIDAD7-DICCIONARIOS/ejercicio9.py
+++ UNIDAD7-DICCIONARIOS/ejercicio9.py
@@ -4,6 +4,7 @@
 # libros2 = {'LibroC': 2, 'LibroD': 4, 'LibroE': 5}
 # Conbina usando el método "update" y muestra su combinación en pantalla
 # Pregunta: importa el orden de combinación?
+# si importa el orden
 
 libros1 = {'LibroA': 1, 'LibroB': 2, 'LibroC': 3}
 libros2 = {'LibroC': 2, 'LibroD': 4, 'LibroE': 5}
