7e523eca70afa4a3f155fe3e3d44c02174fdcea6
tiburon-blanco <antolini.martin@gmail.com>
2022-09-07 11:23:32 -0300

    .

--- UNIDAD8-CONDICIONALES/ejercicio5.py
+++ UNIDAD8-CONDICIONALES/ejercicio5.py
@@ -5,3 +5,12 @@
 # Si el IMC esta entre 18.5 – 24.9 debe indicar: Peso Norma
 # Si el IMC esta entre 25.0 – 29.9 debe indicar: Peso superior al normal
 # Si el IMC es mayor a 30.0 debe indicar: Obesidad
+
+peso=float(input("ingrese peso: "))
+altura=float(input("ingrese altura:"))
+IMC=int(peso/altura)*2
+print("Su indice de masa corporal es: ",IMC)
+
+if IMC<18.5:
+    print("Peso inferior al normal")
+    if 18.5 > IMC <=24.9
