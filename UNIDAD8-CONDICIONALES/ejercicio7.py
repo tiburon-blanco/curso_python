f43e2c55f3f47acf985fb8f996438c45585e16af
tiburon-blanco <antolini.martin@gmail.com>
2022-09-07 13:04:24 -0300

    .

--- UNIDAD8-CONDICIONALES/ejercicio7.py
+++ UNIDAD8-CONDICIONALES/ejercicio7.py
@@ -1,3 +1,19 @@
 # Escribir un programa que solicite al usuario una letra, si es una vocal, muestre el mensaje “es vocal”. 
 # Se debe validar que el usuario ingrese un sólo carácter. 
 # Si ingresa un string de más de un carácter, informarle que no se puede procesar el dato.
+
+vocales=["a","e","i","o","u"]
+
+x=(input("Ingrese una letra: "))
+
+caracteres=len(x)
+
+if caracteres>1:
+    print("debe ingresar solo un caracter:")
+
+if x in vocales:
+    print("es una vocal")
+else:
+    print("no es una vocal")    
+
+
