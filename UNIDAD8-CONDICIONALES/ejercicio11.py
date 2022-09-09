e1cd39c8ce387c66d56ad257b0feb8f5ca92b338
tiburon-blanco <antolini.martin@gmail.com>
2022-09-09 11:58:33 -0300

    .

--- UNIDAD8-CONDICIONALES/ejercicio11.py
+++ UNIDAD8-CONDICIONALES/ejercicio11.py
@@ -18,3 +18,50 @@
 # 3 o mas	25%
 
 # Escribir un programa que pregunte al usuario que tipo de fruta quiere y su cantidad (por kg) e informe el precio que tiene que pagar
+
+print("Ofertas de Banana y Frutilla")
+print("La mercaderia se vende por kilo y solo puede llevar un tipo de fruta")
+
+fruta_1="banana"
+fruta_2="frutilla"
+
+p_banana=100
+desc_ban_1kg= 0
+desc_ban_2kg= 90/100
+desc_ban_3kg= 80/100
+
+
+
+p_frutilla=120
+desc_frut_1kg= 95/100
+desc_frut_2kg= 85/100
+desc_frut_3kg= 75/100
+
+
+fruta=input("Ingrese que tipo de fruta quiere: ")
+kg=int(input("ingrese que cantidad en kg quiere: "))
+
+if fruta != fruta_1 and fruta != fruta_2:
+    print("Esta fruta no esta dentro de las promociones")
+
+
+if fruta_1 in fruta:
+    if kg==1:
+        print("El precio del kg de banana es: ", p_banana*kg)
+    if kg==2:
+        print("El precio del kg de banana es: ", p_banana*kg*desc_ban_2kg)   
+    if kg>=3:
+        print("El precio del kg de banana es: ", p_banana*kg*desc_ban_3kg)
+
+elif fruta_2 in fruta:
+    if kg==1:
+        print("El precio del kg de frutilla es: ", p_frutilla)
+    if kg==2:
+        print("El precio del kg de frutilla es: ", p_frutilla*kg*desc_frut_2kg)   
+    if kg>=3:
+        print("El precio del kg de frutilla es: ", p_frutilla*kg*desc_frut_3kg)     
+else:
+        print("debe ingresar alguna de las dos frutas en ofertas")
+
+
+
