02bd170f8d0a35e0f3cc92b6d071e5e1e594be99
tiburon-blanco <antolini.martin@gmail.com>
2022-09-09 10:57:43 -0300

    ..

--- UNIDAD8-CONDICIONALES/ejercicio10.py
+++ UNIDAD8-CONDICIONALES/ejercicio10.py
@@ -2,3 +2,17 @@
 # El encargado del acceso debe valida esta información previamente.
 # Escribir un programa para el encargado del bar que valide el acceso e indique si dejo o no entrar a la persona.
 # Además indicar el motivo por el cual la persona no accede al bar (En caso que no acceda)
+
+print("Para ingresar al bar debe ser mayor de edad y poseer dos vacunas")
+
+edad_permititda=18
+ingreso_vacunas=2
+
+edad=int(input("Corrobere e ingrese la edad del cliente: "))
+vacunas=int(input("corrobore e ingrese la cantidad de vacunas que posee el cliente: "))
+
+if edad>=edad_permititda and vacunas>=ingreso_vacunas:
+    print("Puede ingresar")
+else:
+    print("no cumple los requisitos legales y/o sanitarios de acceso al local")
+
