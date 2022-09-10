86a68f62d60867a52988f0cca948a3da7d39be9f
tiburon-blanco <antolini.martin@gmail.com>
2022-09-10 19:57:51 -0300

    .

--- UNIDAD9-LOOPS/ejercicio5.py
+++ UNIDAD9-LOOPS/ejercicio5.py
@@ -1,3 +1,10 @@
 # Dado el set
 # frutas = {"pera", "banana", "manzana", "uva", "mango", "kiwi", "melon", "frutilla", "naranja"}
 # Contar la cantidad de frutas que tienen menos de 5 caracteres
+
+
+frutas = {"pera", "banana", "manzana", "uva", "mango", "kiwi", "melon", "frutilla", "naranja"}
+
+for i in frutas:
+    if len(i) < 5:
+       print(i)
