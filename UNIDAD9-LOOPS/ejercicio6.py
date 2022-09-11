a5b7455a7c4461bfb367e31a311699fd1363f23d
tiburon-blanco <antolini.martin@gmail.com>
2022-09-11 12:48:04 -0300

    .

--- UNIDAD9-LOOPS/ejercicio6.py
+++ UNIDAD9-LOOPS/ejercicio6.py
@@ -3,3 +3,20 @@
 # Agregar en OTRA lista aquellas frutas que tienen mas de 5 caracteres
 # Al final imprimir en pantalla esa OTRA lista
 # Recordar que con "append" se agregan elementos a una lista
+
+
+frutas = ["pera", "banana", "manzana", "uva", "mango", "kiwi", "melon", "frutilla", "naranja"]
+
+f_1=[]
+
+for i in frutas:
+    if len(i) > 5:
+        f_1.append(i)
+
+print(f_1)
+
+
+
+       
+
+
