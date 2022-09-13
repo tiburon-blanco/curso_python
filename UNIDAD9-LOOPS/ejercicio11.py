393eea1fa435bd04982e4e8e864de51488722d02
Leo <leocuello@gmail.com>
2022-09-12 23:39:27 -0300

    11

--- UNIDAD9-LOOPS/ejercicio11.py
+++ UNIDAD9-LOOPS/ejercicio11.py
@@ -15,3 +15,21 @@ otro = int(input("Ingrese otro valor a buscar: "))
 edades[pos] = otro
 
 print(edades)
+
+
+
+
+############## EL OCHO ##########################
+
+
+n = [4,6,8,1,6,15,5,20]
+seguir = True
+index = 0
+while seguir:
+
+    value = n[index]
+    print(value)
+    if value > 7:
+        seguir = False
+    index = index + 1
+
