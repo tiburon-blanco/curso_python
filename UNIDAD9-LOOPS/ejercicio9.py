69c85942024a22eed98cf827b8d857cb5b1733e3
tiburon-blanco <antolini.martin@gmail.com>
2022-09-13 09:54:10 -0300

    .

--- UNIDAD9-LOOPS/ejercicio9.py
+++ UNIDAD9-LOOPS/ejercicio9.py
@@ -1,3 +1,11 @@
 # WHILE
 # Se ingresan por teclano números y se van mostrando en pantalla
 # Hasta que se ingrese un 0
+
+
+x=int(input("ingrese un numero por pantalla: "))
+
+while x !=0:
+    x=int(input("numero equivocado, ingrese otro numero por pantalla: "))
+
+print("Acertaste, el numero correcto es cero")
