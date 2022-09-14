03d6e4133578706c162a7e5866c62b4a0d90adef
Leo <leocuello@gmail.com>
2022-09-14 16:07:37 -0300

    FIx

--- UNIDAD4-LISTAS/ejercicio16.py
+++ UNIDAD4-LISTAS/ejercicio16.py
@@ -9,8 +9,8 @@
 
 
 # 1) Mostrar el valor de la poscion [1][1]
-# 2) Mostrar la cantidad de elemtnos que tiene
-# 3) Contar la cantidad de 3 que tiene la fina 2 (Recordar que la primer fila es la 0)
+# 2) Mostrar la cantidad de elemtos que tiene
+# 3) Contar la cantidad de 3 que tiene la fila 2 (Recordar que la primer fila es la 0)
 # 4) Cual es el indice del número 9 de la fila 1
-# 5) Ordernar la primer fila (FIla 0) y mostrar en pantalla la primer fila
+# 5) Ordernar la primer fila (Fila 0) y mostrar en pantalla la primer fila
 # 6) Extender la matriz con la siguiente lista [6,3,7,9,2] y mostrar la matriz
