a92971c93489b85933f987603d51e3d6f0fb0c1b
tiburon-blanco <antolini.martin@gmail.com>
2022-09-14 11:10:48 -0300

    .

--- UNIDAD1-VARIABLE/ejercicio1.py
+++ UNIDAD1-VARIABLE/ejercicio1.py
@@ -1,3 +1,9 @@
+# que es una variable?
+# Variables en Python. En algunos lenguajes de programación, 
+#  #las variables se pueden entender como "cajas" en las que se guardan los datos, 
+# #pero en Python las variables son "etiquetas" que permiten hacer referencia a los datos
+# (que se guardan en unas "cajas" llamadas objetos).
+
 saludo="Hola, mundo"
 print(saludo)
 
--- UNIDAD9-LOOPS/ejercicio11.py
+++ UNIDAD9-LOOPS/ejercicio11.py
@@ -1,11 +1,6 @@
 edades = [5, 14, 7, 23, 31, 5, 32, 19]
 valor = int(input("Ingrese el valor a buscar: "))
 
-#for index, edad in enumerate(edades):
-#    if(edad == valor):
-#        print(index)
-#       break
-
 
 
 pos = edades.index(valor)
@@ -19,17 +14,9 @@ print(edades)
 
 
 
-############## EL OCHO ##########################
+#for index, edad in enumerate(edades):
+#    if(edad == valor):
+#        print(index)
+#       break
 
-
-n = [4,6,8,1,6,15,5,20]
-seguir = True
-index = 0
-while seguir:
-
-    value = n[index]
-    print(value)
-    if value > 7:
-        seguir = False
-    index = index + 1
 
