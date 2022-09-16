ea7391cbab3505cbd2bfb0276d05f8005efb78f3
tiburon-blanco <antolini.martin@gmail.com>
2022-09-16 14:38:09 -0300

    .

--- UNIDAD9-LOOPS/ejercicio11.py
+++ UNIDAD9-LOOPS/ejercicio11.py
@@ -3,3 +3,24 @@
 # Como medida de seguridad, solo hay tres oportunidades para iniciar sesión. 
 # Si hay más de tres oportunidades, se informará al usuario que la clave fue bloqueada
 
+
+name= "root"
+passwd= "citric"
+
+seguir=True   
+intentos=0
+
+while seguir:
+    usuario=input("ingrese su usuario: ")
+    pasword=input("ingrese su passwd: ")
+
+    if usuario==name:
+        if pasword==passwd:
+            print("Datos ingresados correctamente")
+            seguir=False    
+    else:
+        intentos= intentos+1     
+        print("ingrese nuevamente los datos: ") 
+    if intentos==3:
+                print("Clave Bloqueada")    
+                seguir=False   
