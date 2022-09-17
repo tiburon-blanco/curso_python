5f00a1aa0c20b3ff48ff6196133a9d2700d7a31a
tiburon-blanco <antolini.martin@gmail.com>
2022-09-17 17:32:15 -0300

    .

--- UNIDAD9-LOOPS/ejercicio13.py
+++ UNIDAD9-LOOPS/ejercicio13.py
@@ -2,3 +2,22 @@
 # Luego pida esos números, 
 # Después muestre el mayor
 
+
+numeros=int(input("Cuantos numeros piensa ingresar: "))
+
+lista=[]
+contador=0 
+
+seguir=True
+
+while seguir:
+    n=int(input("ingrese un numero: "))
+    lista.append(n)
+    
+    contador=+1
+    if numeros == contador:
+        seguir=False 
+
+            
+
+
