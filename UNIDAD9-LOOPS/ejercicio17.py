c72c7a96a36b482e95e92b51705e6f10ddb0c577
tiburon-blanco <antolini.martin@gmail.com>
2022-09-18 15:00:53 -0300

    .

--- UNIDAD9-LOOPS/ejercicio17.py
+++ UNIDAD9-LOOPS/ejercicio17.py
@@ -7,3 +7,23 @@
 #            [6,3,7,9,2],
 #          ]
 # Contar la cantidad de un número especifico ingresado por teclado
+
+matriz=[ [1,2,4,7,2],
+          [7,9,1,7,8],
+          [4,5,1,3,3],
+          [6,3,7,9,2],
+          ]
+
+n=int(input("Ingrese un numeor a buscar en la matriz: "))
+
+seguir=True
+contador=0
+
+while seguir:
+    if n in matriz:
+        contador=+1
+    print(contador)
+else:
+        seguir=False
+
+
