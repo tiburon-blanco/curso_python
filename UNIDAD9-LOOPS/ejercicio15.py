762dfd06a0a8c04f5c3295a8e6e6813e56e91604
tiburon-blanco <antolini.martin@gmail.com>
2022-09-18 01:13:54 -0300

    .

--- UNIDAD9-LOOPS/ejercicio15.py
+++ UNIDAD9-LOOPS/ejercicio15.py
@@ -1,3 +1,37 @@
 # Escribir un programa que permita al usuario ingresar 6 números enteros, que pueden ser positivos o negativos. 
 # Al finalizar, mostrar la suma de los números negativos y el promedio de los positivos.
 # Hacer con FOR
+
+print("ingrese 6 numeros enteros: ")
+
+lista=[]
+contador=0 
+
+seguir=True
+
+while seguir:
+    if contador==6:
+        seguir=False
+    else:
+        n=int(input("ingrese un numero: "))
+        lista.append(n)
+    contador= contador+1
+
+print(lista)
+
+negativos=[]
+positivos=[]
+
+for i in lista: 
+    if i< 0: 
+       negativos.append(i)
+    else:
+        positivos.append(i)    
+
+print("la suma de los numeros negativos arroja como resultado: ", sum(negativos))
+
+print("el promedio de los numeros positivos es: ", sum(positivos)/ len(positivos))
+
+
+
+
