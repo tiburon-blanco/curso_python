7ee9d0c1440beff77c441c9d09ff95365251f789
tiburon-blanco <antolini.martin@gmail.com>
2022-09-17 23:33:11 -0300

    .

--- UNIDAD9-LOOPS/ejercicio14.py
+++ UNIDAD9-LOOPS/ejercicio14.py
@@ -2,3 +2,20 @@
 # Luego pida esos números, 
 # Después calcule su promedio.
 
+numeros=int(input("Cuantos numeros va a introducir: "))
+
+lista=[]
+contador=0 
+
+seguir=True
+
+while seguir:
+    if numeros==contador:
+        seguir=False
+    else:
+        n=int(input("ingrese esos numeros: "))
+        lista.append(n)
+    contador= contador+1
+
+promedio = sum(lista)/len(lista)
+print("El promedio de los numeros ingresados es: ", promedio)
