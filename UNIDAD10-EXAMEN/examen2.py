3ed2f1f41a04513124b228b1cf6fdfcee66daa1f
Leo <leocuello@gmail.com>
2022-09-20 00:26:53 -0300

    FIX

--- UNIDAD10-EXAMEN/examen2.py
+++ UNIDAD10-EXAMEN/examen2.py
@@ -1,29 +1,16 @@
-# EXAMEN 1
-# Se necesita desarrollar un sistema para un cajero de un supermercado.
-# El sistema deberá procesar la ventas de las personas que se encuentren en la cola.
-# y obtener información al final del proceso.
-# El proceso de pago se hará de una persona por vez, donde, a cada una se le preguntará el nombre y
-# se le procesarán los producto, así hasta el final de la cola.
-# Cada persona puede comprar más de un producto.
-# De cada producto se conoce su código, nombre y precio, la lista de productos es la siguiente:
+# EXAMEN 2
+# El intendente Juan Perez tiene como tarea poner 3 semáforos en su ciudad llamada "La Ideal".
+# "La ideal" es un pueblo de 5 x 5 manzanas donde ocurren muchos accidentes.
+# "La ideal" es una ciudad parecida a "La Plata" donde sus calles se llaman por números
+# La numeración de las 5 cuadras horizontales van del 1 al 5
+# La numeración de las 5 cuadras verticales van del 6 al 10
 
-# Codigo    - Nombre            - Precio
-# 1640      - Mayonesa          - 100
-# 1641      - Dulce de leche    - 120
-# 1642      - Aceite            - 80
-# 1643      - Pan               - 50
-# 1644      - Fideos            - 75 
-# 1645      - Galletitas        - 40
-# 1646      - Cola Cola         - 160
-# 1647      - Queso             - 200
-# 1648      - Fanta             - 150   
-# 1648      - Arroz             - 70
-# 1650      - Mermelada         - 55
+# El problema que tiene el intendente es: en qué calle debe poner los 3 semáforos?
+# Al intendente se le ocurre la siguiente idea:
+# Va a registrar los accidentes que ocurren en cada esquina durante el último mes y
+# en las esquinas más accidentadas va a poner los 3 semáforos.
 
-# El cajero debe preguntar a cada persona por el código del producto y la cantidad del producto que lleva.
-# Si el producto no se encuentra dentro de la lista, se volverá a pedir el código nuevamente.
-# Al finalizar el proceso (una vez procesada toda la cola de personas), se necesitará saber:
+# Por ejemplo, una esquina puede ser: 4 y 7
 
-# A) La cantidad de personas atendidas.
-# B) En nombre de la persona que más dinero gastó, junto con su monto.
-# C) Cuál fue el producto más vendido.
+# Cree un sistema donde se registren accidentes para que al final del mes 
+# el intendente pueda saber en que esquinas deba cocolar los 3 semáforos.
