2c0a12da7c9267bab80db8aba149f5fca820a7d5
tiburon-blanco <antolini.martin@gmail.com>
2022-09-20 20:43:45 -0300

    .

--- UNIDAD09-LOOPS/ejercicio12.py
+++ UNIDAD09-LOOPS/ejercicio12.py
@@ -1,10 +1,20 @@
 # Encuentra la suma de todos los números pares del 1 al 100
 
+contador=0
 
+for i in range(0,100,2):
+        contador=contador+i
+ 
+print("La suma de todos los numero pares del 1 al 100 utilizando una variable contador es: ", contador)
+
+
+print("con listas")
+
+print("con listas")
 numeros_pares=[]
 
 for i in range(0,100,2):
-        numeros_pares.append(i)
+       numeros_pares.append(i)
         
 print(numeros_pares)
 
