36585b6ee310bff403800ca2e27666a20f9587aa
Leo <leocuello@gmail.com>
2022-09-25 12:11:32 -0300

    Examen 3

--- UNIDAD10-EXAMEN/examen3.py
+++ UNIDAD10-EXAMEN/examen3.py
@@ -1,7 +1,7 @@
 # EXAMEN 3
 # Horacio vive en el campo junto con su padre Carlos
 # Se acerca fin de año y Horacio decide vender animales una semana antes de las fiestas.
-# Los animales que venden son 3: patos, chanchos y gallinas
+# Los animales que venden son 3: patos, gallinas, y chanchos 
 # Desarrollar un sistema que le permita a Horacio vender los animales durante 1 semana (5 dias L A V)
 # Sabiendo que al inicio de cada día, su papá Carlos, le informa el stock diario con el que cuenta (Durante el dia)
 # Procesar las ventas de toda la semana (L a V) para informar:
@@ -9,7 +9,12 @@
 # 2) Monto total de todas las ventas
 
 # Tener en cuenta: 
+
 # A: El stock es acumulativo.
 # B: Cada comprador puede comprar más de una animal
 # C: Se debe informar al comprador el monto de la compra
 # D: Informar el comprador si no hay stock del animal que necesita
+# E: Precio x unidad:
+#    Pato = 200 pesos
+#    Gallina = 100 pesos
+#    Chancho = 500 pesos
