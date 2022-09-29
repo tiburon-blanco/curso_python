dccb368b776ce93114aaef13f09c87a8aaac4858
Leo <leocuello@gmail.com>
2022-09-29 20:24:09 -0300

    Examen 4

---
+++ UNIDAD10-EXAMEN/examen4.py
@@ -0,0 +1,74 @@
+precio_pato=200
+precio_gallina=100
+precio_chancho=500
+
+cantidad_animales=0 
+venta_pato = venta_gallina = venta_chancho =0
+stock_pato = stock_gallina = stock_chancho=0
+venta= True
+
+monto_total_pato = monto_total_gallina = monto_total_chancho = 0
+dias=['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
+tipo_animal = ['P', 'G', 'C']
+
+for i in range(5):
+    print("DIA:", dias[i])
+    stock_pato = stock_pato + int(input("Ingrese el stock de patos del dia " +  dias[i]))
+    stock_gallina = stock_gallina + int(input("Ingrese el stock de gallinas del dia " +  dias[i]))
+    stock_chancho = stock_chancho + int(input("Ingrese el stock de chanchos del dia " + dias[i]))
+
+
+    # MIENTRAS ALLA CLIENTE, hacer con WHILE
+
+
+    venta = True
+    while venta:
+        print("Que tipo de animal desea comprar?") 
+  
+        tipo = ''
+        tipo_animal_correcto = True
+        while tipo_animal_correcto:
+            tipo = input("Pato (P), Gallina (G), Chancho (C)")
+            tipo_animal_correcto = True if tipo not in tipo_animal else False
+
+        cantidad = int(input("Que cantidad necesita?"))
+        
+        if tipo == 'P':
+            hay_stock = (stock_pato - cantidad) > 0
+            if hay_stock:
+                stock_pato = stock_pato - cantidad
+                precio = cantidad * precio_pato
+                monto_total_pato = monto_total_pato + precio
+                cantidad_animales = cantidad_animales + 1
+                print("El monto de su compra de patos es: ", precio)
+        if tipo == 'G':
+            hay_stock = (stock_gallina - cantidad) > 0
+            if hay_stock:
+                stock_gallina = stock_gallina - cantidad
+                precio = cantidad * precio_gallina
+                monto_total_gallina = monto_total_gallina + precio
+                cantidad_animales = cantidad_animales + 1
+                print("El monto de su compra de gallina es: ", precio)
+        if tipo == 'C':
+            hay_stock = (stock_chancho - cantidad) > 0
+            if hay_stock:
+                stock_chancho = stock_chancho - cantidad
+                precio = cantidad * precio_chancho
+                monto_total_chancho = monto_total_chancho + precio
+                cantidad_animales = cantidad_animales + 1
+                print("El monto de su compra de chango es: ", precio)
+
+        
+        seguir = input("Desea hacer otra compra (S/N)")
+        if seguir != "S":
+            venta = False
+
+
+
+
+print("Cantidad de animales vendidos: ", cantidad_animales) 
+print("Monto total Pato: ", monto_total_pato)
+print("Monto total Gallina: ", monto_total_gallina)
+print("Monto total Chancho: ", monto_total_chancho)
+monto_total = monto_total_pato + monto_total_gallina + monto_total_chancho
+print("Monto total: ", monto_total)
