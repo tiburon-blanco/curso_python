751214461679141ce0d6c0b0a570c2cb0910b9a3
Leo <leocuello@gmail.com>
2022-09-30 20:52:40 -0300

    func

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio03/modulos/operaciones.py
@@ -0,0 +1,13 @@
+def suma(x, y):
+    return x + y
+
+def resta(x, y):
+    return x - y
+
+def mult(x, y):
+    return x * y
+
+def div(x, y):
+    if(y == 0):
+        return "No se puede"
+    return x / y
---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio03/modulos/ui.py
@@ -0,0 +1,11 @@
+import os
+
+def screen():
+    print("Caculadora Simple")
+    print("1 - SUMA")
+    print("2 - RESTA")
+    print("3 - MULTIPLICAION")
+    print("4 - DIVICION")
+
+def clean():
+  os.system('cls||clear')
