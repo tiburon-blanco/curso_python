11225dd2ea74fdfa0046e100bfdce6f634dbd47b
tiburon-blanco <antolini.martin@gmail.com>
2022-10-08 19:35:01 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio4/registro_usuario.py
@@ -0,0 +1,15 @@
+
+
+registro_usuario={}
+
+nombre =input ("Ingres el nombre de usuario: ")
+apellido= input ("ingrese su apellido: ")
+
+def usuario(nombre, apellido):
+    nombre =input ("Ingrese el nombre de usuario: ")
+    apellido= input ("ingrese su apellido: ")
+    return(usuario)
+
+
+usuario()
+
