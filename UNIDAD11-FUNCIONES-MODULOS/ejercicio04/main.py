707fb26b6d84933970e36bf4606e70af5bfd3449
tiburon-blanco <antolini.martin@gmail.com>
2022-10-08 21:50:52 -0300

    .

--- UNIDAD11-FUNCIONES-MODULOS/ejercicio04/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio04/main.py
@@ -1,1 +1,14 @@
 # Crear un función que reciba 2 valores y retorne cual el mayor
+¶
+def elmayor(n1,n2):
+    if n1>n2:
+        print("el mayor numero ingresado es: ",n1)
+    elif n1<n2:
+        print("el mayor numero ingresado es: ",n2)
+    else:
+        print("Los numeros son iguales, debe ingresar nuevamente: ")
+
+
+    
+elmayor(9,9)
+
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio05/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio05/main.py
@@ -2,3 +2,41 @@
 # Solicitar al usuario que ingrese su dirección email.
 # Imprimir un mensaje indicando si la dirección es válida o no, valiéndose de una función para decidirlo.
 # Una dirección se considerará válida si contiene el símbolo "@".
+
+# email=str(input("ingrese su correo electronico para verificar su validez: "))
+# correo=email
+
+# if "@" in email:
+#     print("el correo es valido")
+# else:
+#     print("el correo es invalido, ingrese nuevamente: ")
+
+email=input("ingrese su correo electronico para verificar su validez: ")
+correo=email
+
+def validacioncorreo(correo):
+    if "@" in email:
+        print("el correo es valido")
+    else:
+        print("el correo es invalido, ingrese nuevamente: ")
+        
+     
+validacioncorreo(correo)
+
+
+
+# validacion=True
+
+# def validacioncorreo(email):
+#     while validacion:
+#         if "@" not in email:
+#             print("el correo es invalido, ingrese nuevamente:")
+            
+#         if "@" in email:
+#             print("el correo es valido")
+#             validacion=False
+            
+     
+# validacioncorreo(email)
+
+
