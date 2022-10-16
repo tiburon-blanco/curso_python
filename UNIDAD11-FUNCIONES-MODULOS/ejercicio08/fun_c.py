79620d21a43583f477e72c3e644b2320f0f5a321
tiburon-blanco <antolini.martin@gmail.com>
2022-10-16 00:39:49 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio08/fun_c.py
@@ -0,0 +1,7 @@
+suma=0
+
+def suma_acumulada(numero):
+    global suma
+    suma = suma + numero
+    return suma
+   
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio08/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio08/main.py
@@ -2,3 +2,28 @@
 # Por cada uno mostrar la suma acumulada 
 # Crear una función que realice dicha suma.
 # La funcion debe recibir el valor y devolver la suma
+
+
+from fun_c import *
+
+
+seguir= True
+
+
+while seguir:
+    numero=int(input("ingrese numeros: "))
+    if numero == 0:
+         seguir = False
+    else:
+        sa= suma_acumulada(numero)
+        print("la suma acumulada es: ", sa)
+    
+
+
+
+    
+
+
+
+    
+        
