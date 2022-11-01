6625f87e6e0e5c3cd6a669edcc574f5e8124e1f7
tiburon-blanco <antolini.martin@gmail.com>
2022-11-01 10:38:25 -0300

    .

--- UNIDAD11-FUNCIONES-MODULOS/ejercicio14/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio14/main.py
@@ -1,2 +1,19 @@
 # Escribir una función que, dado un número de DNI, retorne True si el número es válido y False si no lo es. 
 # Para que un número de DNI sea válido debe tener entre 7 u 8 dígitos.
+
+
+
+def dnivalido():
+
+    dni = input("ingrese su dni: ")
+
+    if len(dni) >= 7 and len(dni) <= 8:
+        print("El dni ingresado es verdadero")
+    else:
+        print("El dni ingresado es invalido")
+
+    return dni
+
+# dni=dnivalido()
+
+dnivalido()
