738da35fcf493b179300c81a4c3e85cd831836a2
tiburon-blanco <antolini.martin@gmail.com>
2022-11-12 09:47:46 -0300

    .

--- UNIDAD11-FUNCIONES-MODULOS/ejercicio12/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio12/main.py
@@ -3,4 +3,19 @@
 # Si la persona es mayor de edad (>=18) se debe guardar el nombre en otra lista llamada "mayores"
 # Se deber crear 2 funciones donde cada una maneje su lista.
 # Al finalizar, imprimir cuantas personas hay en cada lista (menores y mayores)
-# Consejo: crear un módulo (otro archivo) que maneje las funciones y sus listas
+# Consejo: crear un módulo (otro archivo) que maneje las funciones y sus listas True
+
+seguir= True
+
+while seguir:
+    nombre=input("ingrese su nombre: ")
+    edad= int(input("ingrese su edad:"))
+
+
+
+def edad():
+    if edad
+
+def menores():
+
+def mayores():
