cdd1815f848543ff8a69ed97858033a3aa137a9e
tiburon-blanco <antolini.martin@gmail.com>
2022-11-13 10:16:52 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio12/func12.py
@@ -0,0 +1,18 @@
+
+def mayores(nombre, edad):
+    if edad >= 18:
+       lista_mayores={}
+       lista_mayores[nombre]= edad
+       return lista_mayores
+
+def menores(nombre,edad):
+      if edad < 18:
+       lista_menores={}
+       lista_menores[nombre]= edad
+       return lista_menores
+
+
+
+
+
+
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio12/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio12/main.py
@@ -5,17 +5,42 @@
 # Al finalizar, imprimir cuantas personas hay en cada lista (menores y mayores)
 # Consejo: crear un módulo (otro archivo) que maneje las funciones y sus listas True
 
-seguir= True
+from func12 import *
+
+seguir = True
+
+# mayores={}
+
+# menores={}
 
 while seguir:
     nombre=input("ingrese su nombre: ")
     edad= int(input("ingrese su edad:"))
+    mayores(nombre, edad)
+    menores(nombre, edad)
+    cola=input("Hay mas personas para registrar: S/N: ")
+    if cola == "N": 
+        seguir = False
+
+print("la lista de menores es: ", menores)
+
+print("la lista de mayores es: ", mayores)
 
 
 
-def edad():
-    if edad
 
-def menores():
 
-def mayores():
+# while seguir:
+#     nombre=input("ingrese su nombre: ")
+#     edad= int(input("ingrese su edad:"))
+#     if edad >= 18:
+#         mayores[nombre]= edad
+#     else:
+#         menores[nombre]= edad
+#     cola=input("Hay mas personas para registrar: S/N: ")
+#     if cola == "N": 
+#         seguir = False
+
+# print("la lista de menores es: ", menores)
+
+# print("la lista de mayores es: ", mayores)
