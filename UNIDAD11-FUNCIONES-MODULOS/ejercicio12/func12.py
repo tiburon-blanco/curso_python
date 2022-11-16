ce98a66c6382b76d3223e3552492e69cd5f8b946
tiburon-blanco <antolini.martin@gmail.com>
2022-11-16 12:29:20 -0300

    .

--- UNIDAD11-FUNCIONES-MODULOS/ejercicio12/func12.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio12/func12.py
@@ -1,16 +1,31 @@
 
-def mayores(nombre, edad):
-    if edad >= 18:
-       lista_mayores={}
-       lista_mayores[nombre]= edad
-       return lista_mayores
+lista_mayores=[]
+lista_menores=[]
 
-def menores(nombre,edad):
-      if edad < 18:
-       lista_menores={}
-       lista_menores[nombre]= edad
-       return lista_menores
 
+
+def ver_lista_mayores():
+    return lista_mayores
+
+def ver_lista_menores():
+    return lista_menores
+
+def agregar_mayor(nombre):
+       lista_mayores.append(nombre)
+       
+
+def agregar_menores(nombre):
+       lista_menores.append(nombre)
+       
+def ver_informe():
+    cantidad_menores = len(lista_menores)
+    cantidad_mayores = len(lista_mayores)
+    if cantidad_mayores > cantidad_menores:
+        return "mayores"
+    if cantidad_mayores < cantidad_menores:
+        return "menores"
+    if cantidad_mayores == cantidad_menores:
+        return "iguales"
 
 
 
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio12/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio12/main.py
@@ -4,44 +4,33 @@
 # Se deber crear 2 funciones donde cada una maneje su lista.
 # Al finalizar, imprimir cuantas personas hay en cada lista (menores y mayores)
 # Consejo: crear un módulo (otro archivo) que maneje las funciones y sus listas True
+# Necesito saber para invertir si hay mas mayores  o menores.
 
 from func12 import *
 
 seguir = True
 
-ver_mayores=mayores
-ver_menores=menores
-
-# menores={}
-
 while seguir:
     nombre=input("ingrese su nombre: ")
     edad= int(input("ingrese su edad:"))
-    mayores(nombre, edad)
-    menores(nombre, edad)
+    if edad<= 18:
+         agregar_menores(nombre)
+    else:
+        agregar_mayor(nombre)
     cola=input("Hay mas personas para registrar: S/N: ")
     if cola == "N": 
         seguir = False
 
-print("la lista de menores es: ", ver_menores)
+print("la lista de menores es: ", len(ver_lista_menores()))
 
-print("la lista de mayores es: ", ver_mayores)
+print("la lista de mayores es: ", len(ver_lista_mayores()))
+
+informe=ver_informe()
+
+if informe != "iguales":
+    print("el informe final es hay mas: ", informe)
+else:
+    print("el informe final da como resultado que son iguales la cantidad de mayore y menores")
 
 
 
-
-
-# while seguir:
-#     nombre=input("ingrese su nombre: ")
-#     edad= int(input("ingrese su edad:"))
-#     if edad >= 18:
-#         mayores[nombre]= edad
-#     else:
-#         menores[nombre]= edad
-#     cola=input("Hay mas personas para registrar: S/N: ")
-#     if cola == "N": 
-#         seguir = False
-
-# print("la lista de menores es: ", menores)
-
-# print("la lista de mayores es: ", mayores)
