c5029820e8b0fb9ebe014d3f511e787afe913e30
tiburon-blanco <antolini.martin@gmail.com>
2022-11-21 20:15:39 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio17/func_17.py
@@ -0,0 +1,6 @@
+def es_vocal(letra):
+    vocales=["a","e","i","o","u"]
+    if letra in vocales:
+        return True
+    else:
+        return False
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio17/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio17/main.py
@@ -1,4 +1,30 @@
 # Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario
 # devuelve False.
 
+from func_17 import *
+
+letra = input('Introduce una letra por favor: ')
+
+
+vocal=es_vocal(letra)
+
+if vocal == True:
+    print ("la letra ingresada es una vocal.")
+else:
+    print("La letra ingresada NO es una vocal.")
+
+
+
+
+
+
+# def validar_vocal(caracter):
+#     if caracter == ("a" or "e" or "i" or "o" or "u"):
+#         return True
+
+# caracter=input("ingrese un caracter alfabetico: ")
+
+# print("el caracter ingresado es: ", validar_vocal(caracter))  
+
+
 
