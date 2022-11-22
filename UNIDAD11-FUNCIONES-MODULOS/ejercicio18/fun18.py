932a1f218dd929b604d3e593da8022dd234393c8
tiburon-blanco <antolini.martin@gmail.com>
2022-11-22 10:27:16 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio18/fun18.py
@@ -0,0 +1,13 @@
+
+import math
+
+def suma(lis):
+    resultado=sum(lis)
+    return resultado
+
+# lista=int(input("ingrese numeros: "))    
+    
+def mult(list):
+    resultado= math.prod(list)
+    return resultado
+
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio18/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio18/main.py
@@ -2,4 +2,31 @@
 # todos los números de una lista. Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4])
 # La lista se debe carhar por teclado
 
+from fun18 import *
+
+lista_numeros=[]
+
+continuar= True
+
+while continuar:
+    numeros=int(input("ingrese un numero: "))
+    lista= lista_numeros.append(numeros)
+    x= input("Desea ingresar mas numeros: s/n: ")   
+    if x != "s":
+        continuar= False
+
+print("la suma de la lista es", lista_numeros) 
+
+res_suma=suma(lista_numeros)
+
+print("la suma de los numeros ingresados es: ", res_suma)
+
+res_multi= mult(lista_numeros)
+
+print("la suma de los numeros ingresados es: ", res_multi)
+
+
+
+
+
 
