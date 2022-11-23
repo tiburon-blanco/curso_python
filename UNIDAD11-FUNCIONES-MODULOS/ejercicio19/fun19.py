da2d5aedc338b0c6426d34e8a9b60640e7127d0e
tiburon-blanco <antolini.martin@gmail.com>
2022-11-23 11:13:15 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio19/fun19.py
@@ -0,0 +1,22 @@
+
+def inversa_1(cadena):
+    txt=list(cadena)
+    txt.reverse()
+    alverre = " ".join(txt)
+    return alverre
+
+
+def inversa_2(palabra):
+	cadena = palabra[::-1]
+	return cadena
+
+
+
+
+
+
+
+
+
+
+
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio19/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio19/main.py
@@ -1,4 +1,22 @@
-# Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena
+# Definir una función inversa() que calcule la inversión de una cadena. 
+# Por ejemplo la cadena
 # "estoy probando" debería devolver la cadena "odnaborp yotse"
+
+from fun19 import *
+
+cadena=input("Ingrese un texto para la primer funcion reverse: ")
+x= inversa_1(cadena)
+print("1- El resultado de la primer funcion reverse es: ",x)
+
+
+
+otra=input("Ingrese otro texto para evaluar otra alternativa: ")
+y= inversa_2(otra)
+print("el resultado de la segunda funcion  ",y)
+
+
+
+
+
 
 
