fe54ea816c6833e5dc18cff81383de9fac68e4f1
tiburon-blanco <antolini.martin@gmail.com>
2022-11-29 11:15:08 -0300

    .

---
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio25/fun25.py
@@ -0,0 +1,8 @@
+def contar_vocales(palabra):
+    a=palabra.count("a")
+    e=palabra.count("e")
+    i=palabra.count("i")
+    o=palabra.count("o")
+    u=palabra.count("u")
+    cantidad_vocales= print("La letra a se encuentra: ", a ,"veces", "la letra e: ", e,  "Letra i", i, "Letra o", o, "Letra u", u) 
+    return cantidad_vocales
--- UNIDAD11-FUNCIONES-MODULOS/ejercicio25/main.py
+++ UNIDAD11-FUNCIONES-MODULOS/ejercicio25/main.py
@@ -2,3 +2,11 @@
 # que reciba una palabra y cuente 
 # cuantas letras "a" tiene, cuantas letras "e" tiene 
 # y así hasta completar todas las vocales.
+
+from fun25 import*
+
+palabra= input("ingrese una palabra para poder contar sus vocales: ")
+
+resultado=contar_vocales(palabra)
+
+print(resultado)
