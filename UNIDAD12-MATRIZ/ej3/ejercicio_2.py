000ed82361b90e85969590e680130a487974027a
Leo <leocuello@gmail.com>
2023-05-23 11:27:50 -0300

    FIX

---
+++ UNIDAD12-MATRIZ/ej3/ejercicio_2.py
@@ -0,0 +1,6 @@
+## Crear una batalla naval de 1 solo jugador
+## Tablero de 5x5
+## Poner 2 barcos de 2x2 (Azar)
+## Fin de juego: cuando elimine los 2 barcos o cuando llegue a 20 tiros o bombas tiradas.
+## Indicar en cada tipo si fue agua, tocado o hundido 
+## Indicar si gano o perdio
