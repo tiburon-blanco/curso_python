da848110352b296418a1b235f1a5827d71f2eae1
Leo <leocuello@gmail.com>
2023-05-23 10:57:19 -0300

    FIX

---
+++ UNIDAD12-MATRIZ/ej2/ejercicio_2.py
@@ -0,0 +1,4 @@
+## Crear un amtrix de X x Y con 0 (Ej 5x5)
+## Inicializar con 5 valores con 1
+## Comienza el juego, Se le pide al usuario que ingrese la pos X e Y y decir si en esa posicion se encuentra un 1.
+## El juego termina cuando se encuentra un 1 o llego a 5 posibilidades de error.
