e26b8866cd499574fe20bb2a29a24ef9c4b60864
tiburon-blanco <antolini.martin@gmail.como>
2023-06-28 13:27:03 -0300

    fix

--- UNIDAD16POO/modules/moto.py
+++ UNIDAD16POO/modules/moto.py
@@ -1,12 +1,14 @@
-from maquina import Maquina
+from UNIDAD16POO.modules.maquina import Maquina
 
 
 class Moto(Maquina):
-
     def consumo(self, litros):
-        self.consumo = litros/0.5*10
-        print("la autonomia de la moto con esta cantida de litros es de: ",
-              self.consumo, "metros.")
+        self.consumo = litros / 0.5 * 10
+        print(
+            "la autonomia de la moto con esta cantida de litros es de: ",
+            self.consumo,
+            "metros.",
+        )
 
 
 moto = Moto("vw", "azul", 10)
